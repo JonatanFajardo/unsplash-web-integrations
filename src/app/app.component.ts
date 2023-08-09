2ab0ba302c6072ae2b45464869a364a3fb78a9f7
Jonatan Fajardo <69583436+JonatanFajardo@users.noreply.github.com>
2023-08-08 23:57:46 -0600

    Ahora se puede buscar imagenes desde la interfaz archivos restantes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,42 @@ import { RootResult } from './models/RootResult';
 })
 export class AppComponent {
   title = 'unsplash-web-integrations';
+  // search:string="";
+
+  constructor(private unsplashService: UnsplashService){}
+  // url:string = `{}`;
+  resultado:any=[];
+
+  showModal = false;
+
+  openModal() {
+    this.showModal = true;
+  }
+
+  closeModal() {
+    this.showModal = false;
+  }
+
+  buscarImagen(search:string){
+    this.unsplashService.obtenerImagenes(search).subscribe((datos)=>{
+      this.resultado = datos;
+      console.log(datos.results);
+      console.log(this.resultado.results[0].urls['regular']);
+      console.log(this.resultado.results[1].urls['regular']);
+      
+      
+    });
+  }
+
+
   
+  // mostrarModal(itemId: string) {
+  //   const modalSelector = `#modal-${itemId}`;
+  //   const modalElement = document.querySelector(modalSelector);
+  //   if (modalElement) {
+  //     const bootstrapModal = new bootstrap.Modal(modalElement);
+  //     bootstrapModal.show();
+  //   }
+  // }
+
 }
